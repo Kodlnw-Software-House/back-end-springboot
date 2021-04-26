332a251b7a895d517e048f8dec7a1ba12ccba88d
gun082544 <gun082544@hotmail.com>
2021-04-26 23:48:58 +0700

    [DELETE] Spring boot test

--- src/test/java/com/int221/finalproject/FinalprojectApplicationTests.java
+++ src/test/java/com/int221/finalproject/FinalprojectApplicationTests.java
@@ -3,11 +3,6 @@ package com.int221.finalproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FinalprojectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
